e1cd3551733fce455ad07efcffe19a32e9f9c248
Haribou Abdallah <haribou@localhost.localdomain>
2019-12-12 18:01:15 +0100

    Modification du fichier planning_db.sql

--- planning_db.sql
+++ planning_db.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.18, for Linux (x86_64)
 --
--- Host: localhost    Database: planning_db
+-- Host: localhost    Database: scolaire
 -- ------------------------------------------------------
 -- Server version	8.0.18
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -102,7 +102,8 @@ UNLOCK TABLES;
 -- Table structure for table `Etudiant`
 --
 
-DROP TABLE IF EXISTS `Etudiant`; /*!40101 SET @saved_cs_client     = @@character_set_client */;
+DROP TABLE IF EXISTS `Etudiant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Etudiant` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -235,31 +236,6 @@ INSERT INTO `Matiere_Enseignant` VALUES (4,1,32),(5,2,34),(6,4,4);
 UNLOCK TABLES;
 
 --
--- Table structure for table `personne`
---
-
-DROP TABLE IF EXISTS `personne`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `personne` (
-  `id` int(10) unsigned NOT NULL,
-  `nom` varchar(40) NOT NULL,
-  `prenom` varchar(40) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `personne`
---
-
-LOCK TABLES `personne` WRITE;
-/*!40000 ALTER TABLE `personne` DISABLE KEYS */;
-INSERT INTO `personne` VALUES (1926046598,'Jacquie','Pierre');
-/*!40000 ALTER TABLE `personne` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `Reservation`
 --
 
@@ -358,6 +334,7 @@ CREATE TABLE `Seance` (
   `id_matiere` int(11) DEFAULT NULL,
   `id_enseignant` int(10) unsigned DEFAULT NULL,
   `id_Salle` int(10) unsigned DEFAULT NULL,
+  `etat` varchar(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -370,6 +347,31 @@ LOCK TABLES `Seance` WRITE;
 /*!40000 ALTER TABLE `Seance` DISABLE KEYS */;
 /*!40000 ALTER TABLE `Seance` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `personne`
+--
+
+DROP TABLE IF EXISTS `personne`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `personne` (
+  `id` int(10) unsigned NOT NULL,
+  `nom` varchar(40) NOT NULL,
+  `prenom` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personne`
+--
+
+LOCK TABLES `personne` WRITE;
+/*!40000 ALTER TABLE `personne` DISABLE KEYS */;
+INSERT INTO `personne` VALUES (1926046598,'Jacquie','Pierre');
+/*!40000 ALTER TABLE `personne` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -380,4 +382,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-12-10 17:15:17
+-- Dump completed on 2019-12-12 18:00:52
